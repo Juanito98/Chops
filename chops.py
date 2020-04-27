7535a60241cd409eed0db0f222bc4aa65bd413ee
Juan Carlos Sigler Priego <jcsiglerp@MacBook-Pro-de-Juan.local>
2020-04-26 19:15:45 -0500

    Add gifs

--- chops.py
+++ chops.py
@@ -2,17 +2,47 @@ import discord
 import random
 import asyncio
 import os
+import giphy_client
 from discord.ext import commands
 from dotenv import load_dotenv
+from giphy_client.rest import ApiException
 
 load_dotenv()
+discord_token = os.environ['DISCORD_TOKEN']
+giphy_token = os.environ['GIPHY_TOKEN']
 
 bot = discord.Client()
+giphy_api_instance = giphy_client.DefaultApi()
 
 bot = commands.Bot(command_prefix='-chops ')
 
+def search_gifs(query):
+    try:
+        return giphy_api_instance.gifs_search_get(giphy_token, query, limit=5, rating = 'g')
+    except ApiException as e:
+        return "Exception when calling DefaultApi->gifs_search_get: %s\n" % e
+
+def random_gif():
+    response = giphy_api_instance.gifs_random_get(giphy_token)
+    gif = response.data
+    return gif.url
+
+def gif_response(emotion):
+    gifs = search_gifs(emotion)
+    lst = list(gifs.data)
+    gif = random.choices(lst)
+    return gif[0].url
+
+@bot.command(name='gif')
+async def gifCmd(ctx, msg=""):
+    if msg == "":
+        gif = random_gif()
+    else:
+        gif = gif_response(msg)
+    await ctx.send(gif)
+
 @bot.command(name='foto')
-async def foto(ctx):
+async def fotoCmd(ctx):
     files = [
         'chops1.jpeg',
         'chops2.jpeg',
@@ -30,14 +60,15 @@ async def foto(ctx):
     await ctx.send(msg, file=file)
 
 @bot.command(name='saluda')
-async def saluda(ctx):
+async def saludaCmd(ctx):
     saludos = [
         'Hola prro! :dog:',
         'Kyc y deme comida :canned_food:',
-        'Tú no me mandas :angry:'
+        'Tú no me mandas :angry:',
     ]
-    response = random.choice(saludos)
-    await ctx.send(response)
+    saludo = random.choice(saludos)
+    await ctx.send(saludo)
+    await ctx.send(gif_response(saludo))
 
 
 @bot.event
@@ -47,4 +78,4 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
-bot.run(os.environ['DISCORD_TOKEN'])
+bot.run(discord_token)
